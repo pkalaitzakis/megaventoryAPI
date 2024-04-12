2db624f02aa398418acb462a5d5be58449e05c61
pkalaitzakis <56133753+pkalaitzakis@users.noreply.github.com>
2024-04-12 20:41:05 +0300

    Delete Models/ProductSupplierClient.cs
    Left over, not functional code

--- Models/ProductSupplierClient.cs
+++
@@ -1,9 +0,0 @@
-public class ProductSupplierClient {
-    private SupplierClient supplierClient;
-    private Product product;
-    public SupplierClient SupplierClient {
-        get {return supplierClient;}
-        set {supplierClient = value;}
-    }
-
-}
